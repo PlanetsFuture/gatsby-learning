31937066763c003ffceec5b0afd30e8ac944b1f1
Danny Ramírez <danny.ramrez7@gmail.com>
2022-11-07 16:42:50 -0600

    part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
-
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p> "Im following Gatsby tutorial</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown potbull"
+        src="../images/coffee1.jpg"
+      />
     </Layout>
   )
 }
